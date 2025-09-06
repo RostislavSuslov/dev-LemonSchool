82a9aac8071344eed6591642f44070e14af5c2fb
RostSuslov <suslovros@gmail.com>
2025-09-06 11:17:15 +0300

    feat: header done

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const getLoremImage = () => {
+    const images = document.querySelectorAll('img');
+    
+    images.forEach(item => {
+        if (!item.getAttribute('src')) {
+            fetch('https://picsum.photos/640/360')
+                .then(response => response.blob())
+                .then(blob => item.src = URL.createObjectURL(blob))
+                .catch(error => item.src = "assets/img/cat-scottish.webp");
+        }
+    });
+};
+
+const burgerBtn = document.querySelector('#burger');
+const mobileContainer = document.querySelector('#mobile-container');
+
+burgerBtn.addEventListener('click', ()=> {
+    mobileContainer.classList.toggle('hidden')
+    burgerBtn.classList.toggle('after:bg-purple')
+    burgerBtn.classList.toggle('before:bg-purple')
+})
+
+
+document.querySelectorAll('img').length > 0 ? getLoremImage() : null;
