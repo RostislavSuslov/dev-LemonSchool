27bec968c0d6085d8ec8afda395b0a75b8e40acc
RostSuslov <suslovros@gmail.com>
2025-10-01 20:50:58 +0300

    feat: modal window

--- src/modal.js
+++ src/modal.js
@@ -1,17 +1,33 @@
-window.addEventListener('load', ()=> {
-    
-    const initModalWindow = ()=> {
-        const triger = document.querySelector('#triger')
-        const modal = document.querySelector('.modal')
-        const closeWindow = document.querySelector('#close-window')
-        
-        triger.addEventListener('click', ()=> {
-            modal.classList.add('show')
+window.addEventListener('load', () => {
+
+    const initModalWindow = () => {
+        const modalShowTrigers = document.querySelectorAll('[data-triger]')
+        const modalCloseTriger = document.querySelectorAll('[data-close-modal]')
+
+        modalShowTrigers.forEach(triger => {
+            triger.addEventListener('click', () => {
+                const trigerData = triger.getAttribute('data-triger')
+                document.getElementById(trigerData).classList.add('show')
+            })
+        }
+        )
+
+        modalCloseTriger.forEach(button => {
+            button.addEventListener('click', () => {
+               // document.querySelector('.modal.show')
+                button.closest('.modal').classList.remove('show')
+                
+                
+            })
         })
 
-        closeWindow.addEventListener('click', ()=> {
-            modal.classList.remove('show')
-        })
+        // triger.addEventListener('click', ()=> {
+        //     modal.classList.add('show')
+        // })
+
+        // closeWindow.addEventListener('click', ()=> {
+        //     modal.classList.remove('show')
+        // })
     }
 
     initModalWindow()
